294f1eb9a6c6ff5202cc23b4f57af259a4cb11d2
CaioLima42 <caio.lima@ges.inatel.br>
2025-05-21 18:28:57 -0300

    Correcting View Problems

--- Case_Pratico/classifier/views.py
+++ Case_Pratico/classifier/views.py
@@ -1,19 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+from PyPDF2 import PdfReader
 
 class ProcessedEmail:
     personName: str
     relevant: bool
     email: str
 
-
-def iaProcess(email: str) -> str:
+def iaProcess(email: str, personName: str) -> str:
     #TODO make the ia process to classifier the email
-    pass
 
-def EmailReader(request: HttpResponse):
+    return "Classificado"
+
+def renderHomePage(request: HttpRequest):
+    return render(request, 'index.html')
+
+def receiveEmail(request: HttpRequest) -> HttpResponse:
+    text = ""
     if request.method == "POST":
-        email = request.POST.get('email')
-        personName = request.POST.get('name')
-        iaProcess(email, personName)
+        if 'pdf_file' in request.FILES:
+            pdf_file = request.FILES['pdf_file']
+            reader = PdfReader(pdf_file)
+            text = "\n".join(page.extract_text() for page in reader.pages if page.extract_text())
+
+        elif 'txt_file' in request.FILES:
+            txt_file = request.FILES['txt_file']
+            text = txt_file.read().decode('utf-8')
+
+        elif 'email' in request.POST:
+            # Exemplo de coleta dos dados de texto simples
+            nome = request.POST.get('nome')
+            email = request.POST.get('email')
+            text = f"Nome: {nome}\nEmail: {email}"
+        return render(request, 'index.html', {'text': text})
+    return render(request, 'index.html')
 
