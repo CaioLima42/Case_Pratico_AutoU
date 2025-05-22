56717c610759c2cac5a445101e204391bd16e5bb
CaioLima42 <caio.lima@ges.inatel.br>
2025-05-22 10:46:26 -0300

    Correcting Models

--- Case_Pratico/classifier/models.py
+++ Case_Pratico/classifier/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class CheckedEmail(models.Model):
+    clientName = models.CharField(max_length=100, verbose_name="User Name")
+    emailMesage = models.TextField(verbose_name="Email Mensage")
+    relevence = models.BooleanField(default=False, verbose_name="Its Relevant?")
+    response = models.TextField(verbose_name="Respose for email")
+
+    class Meta:
+        verbose_name = "CheckedEmail"
+        verbose_name_plural = "CheckedEmail"
+        ordering = ['-clientName']
+
+    def __str__(self):
+        return f"The email of {self.nome_usuario} ({'Is relevant' if self.relevancia else 'is not relevant'})"
