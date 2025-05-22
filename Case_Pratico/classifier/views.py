967ad40d7bb5127c5fbd4b6e350379bb9bea6d8d
CaioLima42 <caio.lima@ges.inatel.br>
2025-05-22 10:53:09 -0300

    Update views

--- Case_Pratico/classifier/views.py
+++ Case_Pratico/classifier/views.py
@@ -1,38 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 from PyPDF2 import PdfReader
-from dataManipulation import removeStopWords, stemmingWords
+from .dataManipulation import removeStopWords, stemmingWords
+from .aiProcess import checkImportance, ResponseMensage
 
 class ProcessedEmail:
-    personName: str
-    relevant: bool
-    email: str
+    def __init__(self, personName: str, relevant: bool, email: str, response: str):
+        self.personName = personName
+        self.relevant = relevant
+        self.email = email
+        self.response = response
 
-def ProcessMensage(email: str, personName: str) -> str:
-    #TODO make the ia process to classifier the email
+def ProcessMensage(personName: str, email: str) -> ProcessedEmail:
     emailProsseced = removeStopWords(email)
     emailProsseced = stemmingWords(emailProsseced)
-    return "Classificado"
-
+    relevance = checkImportance(emailProsseced)
+    if not relevance:
+        return ProcessedEmail(personName, relevance, email, "")
+    response = ResponseMensage(email, personName)
+    return ProcessedEmail(personName, relevance, email, response)
+    
 def renderHomePage(request: HttpRequest):
     return render(request, 'index.html')
 
 def receiveEmail(request: HttpRequest) -> HttpResponse:
     text = ""
     if request.method == "POST":
+        name = request.POST.get('nome')
         if 'pdf_file' in request.FILES:
             pdf_file = request.FILES['pdf_file']
             reader = PdfReader(pdf_file)
-            text = "\n".join(page.extract_text() for page in reader.pages if page.extract_text())
+            #text = "\n".join(page.extract_text() for page in reader.pages if page.extract_text())
 
         elif 'txt_file' in request.FILES:
             txt_file = request.FILES['txt_file']
             text = txt_file.read().decode('utf-8')
 
         elif 'email' in request.POST:
-            nome = request.POST.get('nome')
-            email = request.POST.get('email')
-            text = f"Nome: {nome}\nEmail: {email}"
-        return render(request, 'index.html', {'text': text})
-    return render(request, 'index.html')
+            text = request.POST.get('email')
+            #text = f"Nome: {name}\nEmail: {email}"
+        responseMensage = ProcessMensage(name, text)
+        return render(request, 'simple_response.html', {'text': responseMensage.relevant, 'dirt': responseMensage.response})
+    return render(request, 'simple_response.html')
 
