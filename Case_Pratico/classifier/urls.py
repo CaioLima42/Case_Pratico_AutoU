fcb890212da5a23a91065d449a556a7933a2317d
CaioLima42 <caio.lima@ges.inatel.br>
2025-05-23 12:49:20 -0300

    add new urls

--- Case_Pratico/classifier/urls.py
+++ Case_Pratico/classifier/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.renderHomePage, name='home'),
-    path('enviar/', views.receiveEmail, name='receive_email'),
+    path('', views.renderHomePage, name='home_page'),
+    path('ler/', views.renderSendEmail, name='read_email'),
+    path('checar', views.receiveEmail, name='check_email'),
+    path('enviar/', views.sendEmail, name='send_email'),
+    path('ver/', views.renderAllEmails, name='see_email'),
+    path('drop/', views.dropDataset, name='dropTable'),
+    path('deletar/<int:email_id>/', views.deleteEmail , name='delete_email'),
+    path('editar/<int:email_id>/', views.editEmail, name='edit_email')
 ]
